bbe3ccb394b4e036e798ae6d68df7ac3c86406f1
Nitinweb <nitinweb@users.noreply.github.com>
2017-05-23 23:29:40 -0400

    Initial Commit
    Top 20 Pizza Order By Toppings

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaToppingCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var query = DataUtils.GetTop20Toppings();
+            foreach (var result in query)
+            {
+                Console.WriteLine("Topping Name: {0} - Count: {1}", result.Name, result.Count);
+            }
+        }
+    }
+}
